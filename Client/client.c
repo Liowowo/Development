3d34c0dc9821f1e4f9ee6a62edc08fc0a46a7498
Liowowo <lioloncin@gmail.com>
2022-08-04 15:11:17 +0200

    V1.0

--- Client/client.c
+++ Client/client.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-// Library for the sockets
-#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
-#include <unistd/h>
 #include <arpa/inet.h>
+
+//void exchange (int client_socket){
+
+int main(void){
+
+	//Configuration of IP address & port number
+	char *ip = "127.0.0.1";
+	int port = 4444;
+
+	int client_socket;
+
+	struct sockaddr_in addr;
+	socklen_t addr_size;
+	
+	client_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (client_socket < 0){
+		perror ("[-] Socket error");
+		exit (1);
+	}
+
+	memset(&addr, '\0', sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = port;
+	addr.sin_addr.s_addr = inet_addr(ip);
+
+	connect(client_socket, (struct sockaddr*)&addr, sizeof(addr));
+	printf("[+] Connected to the server.\n");
+
+	exit(0);
+}
--- Server/server.c
+++ Server/server.c
@@ -5,31 +5,58 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+#define MAXSIZE 1024
+
+void exchange (int client_socket){
+
+	char buffer[MAXSIZE];
+	char string_msg[] = "";
+	FILE *output;
+
+	// Loop for exchange between Client & Server
+	while(1){
+
+		bzero(buffer,MAXSIZE);
+
+		// Read and convert the socket from the client
+		int bytes_of_socket = recv(client_socket, buffer, sizeof(buffer),0);
+		strncpy(string_msg, buffer, bytes_of_socket);
+		string_msg[bytes_of_socket] = '\0';
+
+		// Execute Linux command & create an output of this command
+		output = popen (string_msg, "r");
+		if (output = NULL){
+			printf("Failed to run command, try again ! \n");
+			exit(1);
+		}
+	}
+}
+
 int main() {
 
-	//Initiazation of ip adress & port number
-	const char *ip = "127.0.0.1";
-	const int port = 4444;
+	//Configuration of IP address & port number
+	char *ip = "127.0.0.1";
+	int port = 4444;
 
-	int server_socket,client_socket;
+	int n;
+
+	int server_socket, client_socket;
 
 	struct sockaddr_in server_addr, client_addr;
 	socklen_t addr_size;
-	char buffer[1024];
-	int n;
-
+	
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_socket < 0){
 		perror ("[-] Socket error");
 		exit (1);
 	}
 
-	printf("[+] TCP server socket started.\n");
-
 	memset(&server_addr, '\0', sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = port;
 	server_addr.sin_addr.s_addr = inet_addr(ip);
+
+	printf("[+] Server started. \n");
 
 	n = bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
 	if (n < 0){
@@ -43,14 +70,9 @@ int main() {
 
 	addr_size = sizeof(client_addr);
 	client_socket = accept(server_socket, (struct sockaddr*)&client_addr,&addr_size);
-	printf("[+] Client connecte.\n");
+	printf("[+] Client connected !\n");
 	
-	addr_size = sizeof(client_addr);
-		client_socket = accept(server_socket, (struct sockaddr*)&client_addr,&addr_size);
-		printf("[+] Client connecte.\n");
+	exchange(client_socket);
 
-	//while (1){
-		
-	//}
 	return 0;
 }
