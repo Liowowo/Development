a1dcaf5ebc1034577963ccd8749a48ee04355897
Liowowo <lioloncin@gmail.com>
2022-08-09 15:02:28 +0200

    Adding comment on the code

--- Client/client.c
+++ Client/client.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
+#include <stdio.h> // for "printf()"
+#include <stdlib.h> // for "atoi()"
+#include <string.h> // for "bzero()"
+#include <unistd.h> // for "close()"
+#include <sys/socket.h> // for "socket(), connect(), send() and recv()"
+#include <arpa/inet.h> // for "sockaddr_in and inet_addr()"
 
 #define MAXSIZE 9192
 
@@ -13,21 +13,24 @@ int main(int argc, char *argv[]){
 	int port = atoi(argv[1]);
 	char *ip = argv[2];
 
+	// Declaration of variables for the exchange
 	char cmd[MAXSIZE];
 	char strings_msg[MAXSIZE+1];
 
+	// Declaration of variables for the socket programming
 	int client_socket, client_state;
-
 	struct sockaddr_in addr;
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(port);
 	addr.sin_addr.s_addr = inet_addr(ip);
 
+	// Check if the user put the arguments
 	if (argc != 3) {
          printf("You need to specify port number and IP address ! \n");
          exit(1);
      }
-	
+
+	// Initialization of the socket
 	client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
 	if (client_socket < 0){
@@ -35,6 +38,7 @@ int main(int argc, char *argv[]){
 		exit (1);
 	}
 
+	// Check if the client can connect with the server
 	if ((client_state = connect(client_socket, (struct sockaddr*)&addr, sizeof(addr))) < 0) {
 		printf("[-] Connection Failed ! Wrong port number or IP address. \n");
 		return -1;
@@ -45,21 +49,25 @@ int main(int argc, char *argv[]){
 	// Loop for exchange between Client & Server
 	while(1){
 
+		// Put the value of the memory at 0
 		bzero(cmd, MAXSIZE);
 
 		printf("[+] Enter a command : ");
 
+		// Check if the command = at "quit" for stopping and closing the connection
 		if (strcmp(fgets(cmd, MAXSIZE, stdin),"quit\n") == 0){
-			printf("[+} Connection with the server closed !\n");
+			printf("[+] Connection with the server closed !\n");
 			close(client_socket);
 			exit(0);
 		}
 
+		// Send the command to the server
 		else {
 			printf("\n");
 			send (client_socket, cmd, strlen(cmd), 0);
 			sleep(1);
 
+			// Receive the result of the command
 			int bytes_of_socket  = recv(client_socket, cmd, sizeof(cmd),0);
 			strncpy(strings_msg, cmd, bytes_of_socket);
 			strings_msg[bytes_of_socket] = '\0';
--- Server/server.c
+++ Server/server.c
@@ -1,46 +1,52 @@
 #include <stdio.h> // for "printf()"
 #include <stdlib.h> // for "atoi()"
-#include <string.h> // for "memset()"
+#include <string.h> // for "bzero()"
 #include <unistd.h> // for "close()"
 #include <sys/socket.h> // for "socket(), connect(), send() and recv()"
-#include <arpa/inet.h> //for "sockaddr_in and inet_addr()"
+#include <arpa/inet.h> // for "sockaddr_in and inet_addr()"
 
-#define MAXSIZE 9192
+#define MAXSIZE 9192 
 
 int main(int argc, char *argv[]){
 
-	//Configuration of IP address & port number
+	// Initialization of IP address & port number
 	int port = atoi(argv[1]);
 	char *ip = argv[2];
 
+	// Declaration of variables for the exchange
 	char cmd[MAXSIZE];
 	char buffer[MAXSIZE];
 	char strings_msg[MAXSIZE+1];
-
 	FILE *output;
 
+	// Declaration of variables for the socket programming
 	int server_socket, client_socket, binding;
-
 	struct sockaddr_in server_addr, client_addr;
 	int n, len;
 
+	// Inititialization of variables use for the network
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port);
+	server_addr.sin_addr.s_addr = inet_addr(ip);
+
+
+	// Check if the user put the arguments
 	if (argc != 3) {
          printf("You need to specify port number and IP address ! \n");
          exit(1);
      }
 	
+	// Initialization of the socket
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
 	if (server_socket < 0){
 		perror ("[-] Socket error");
 		exit (1);
 	}
 
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(port);
-	server_addr.sin_addr.s_addr = inet_addr(ip);
-
 	printf("[+] Server started. \n");
 
+	// Bind the server with the port number and the IP address specify
 	binding = bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
 
 	if (binding < 0){
@@ -59,6 +65,7 @@ int main(int argc, char *argv[]){
 
 		printf("listening... \n");
 
+		// Accept the connection with the client
 		len = sizeof(client_addr);
 		client_socket = accept(server_socket, (struct sockaddr*)&client_addr,&len);
 
@@ -70,9 +77,10 @@ int main(int argc, char *argv[]){
 		printf("[+] Client connected from %s ! \n",inet_ntoa(client_addr.sin_addr));
 	}
 
-	// Loop for exchange between Client & Server
+	// Infinite loop for exchange between Client & Server
 	while(1){
 
+		// Put the value of the memory at 0
 		bzero(cmd,MAXSIZE);
 		bzero(buffer,MAXSIZE);
 
@@ -81,7 +89,7 @@ int main(int argc, char *argv[]){
 		strncpy(strings_msg, cmd, bytes_of_socket);
 		strings_msg[bytes_of_socket] = '\0';
 
-		// Execute Linux command & create an output of this command
+		// Execute bash command & create an output of this command
 		output = popen (strings_msg, "r");
 		if (output == NULL){
 			fputs("[+] Connexion with client closed ! \n", stderr);
@@ -89,6 +97,7 @@ int main(int argc, char *argv[]){
 			exit(0);
 		}
 
+		// Send to the client the result of the command
 		while (fgets(buffer, MAXSIZE-1, output) != NULL){
 			send(client_socket, buffer, strlen(buffer), 0);
 			sleep(0.01);
